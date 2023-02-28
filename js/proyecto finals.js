ea8ffa4cab42ec6fa830a661123dbf77f3af2fb2
emilio galarza <galarzakpo@hotmail.com>
2023-02-28 02:36:43 -0300

    Primer Commit

---
+++ js/proyecto finals.js
@@ -0,0 +1,5 @@
+
+document.getElementById("boton").addEventListener('click', function() {
+    console.log("Proyecto final Front End");
+    document.getElementById("demo").innerHTML = "Mi nombre es Emilio, nacido en Argentina-Tucuman y mi interes por la pc y la programacion surgio cuando apenas tenia 13 años, tuve mi primer pc a los 15 y fue cuando tuve mas tiempo para estar metido en todo este mundo de la programacion. Soy una persona que cuando se interesa en algo lo consigue y lo que quiero es vivir de esto que es la programacion dando lo mejor de mi."
+});
